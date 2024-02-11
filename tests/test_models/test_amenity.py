2b58c4e24b0ce7fbe8b2a430212b2a90c3767688
Anashm24 <anashoumaid19@gmail.com>
2024-02-11 14:06:15 +0100

    new

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import unittest
+import models
+from models.amenity import Amenity
+from datetime import datetime
+import os
+
+
+class TestAmenityModel(unittest.TestCase):
+    def setUp(self):
+        self.amenity_model = Amenity()
+
+    def tearDown(self):
+        del self.amenity_model
+
+    def test_instance_creation(self):
+        self.assertIsInstance(self.amenity_model, Amenity)
+        self.assertTrue(hasattr(self.amenity_model, 'id'))
+        self.assertTrue(hasattr(self.amenity_model, 'created_at'))
+        self.assertTrue(hasattr(self.amenity_model, 'updated_at'))
+        self.assertTrue(hasattr(self.amenity_model, 'name'))
+
+    def test_string_representation(self):
+        string_repr = str(self.amenity_model)
+        self.assertIn("[Amenity]", string_repr)
+        self.assertIn("id", string_repr)
+        self.assertIn("created_at", string_repr)
+        self.assertIn("updated_at", string_repr)
+        self.assertIn("name", string_repr)
+
+if __name__ == '__main__':
+    unittest.main()
