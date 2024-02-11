6a96787bd4ea2d1c3c2dde964f82f62b00753a7c
Anashm24 <anashoumaid19@gmail.com>
2024-02-11 14:03:36 +0100

    new

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -6,9 +6,7 @@ Unittest classes:
     TestUser_save
     TestUser_to_dict
 """
-import os
 import models
-from models.base_model import BaseModel
 from models.user import User
 import unittest
 from datetime import datetime
@@ -18,54 +16,58 @@ from time import sleep
 class TestUser_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the User class."""
 
-    def test_no_args_instantiates(self):
-        self.assertEqual(User, type(User()))
+    def setUp(self):
+        self.user_model = User()
 
-    def test_new_instance_stored_in_objects(self):
-        self.assertIn(User(), models.storage.all().values())
-
-    def test_id_is_public_str(self):
-        self.assertEqual(str, type(User().id))
-
-    def test_created_at_is_public_datetime(self):
-        self.assertEqual(datetime, type(User().created_at))
+    def tearDown(self):
+        del self.user_model
+    
+    def test_instance_creation(self):
+        self.assertIsInstance(self.user_model, User)
+        self.assertTrue(hasattr(self.user_model, 'id'))
+        self.assertTrue(hasattr(self.user_model, 'created_at'))
+        self.assertTrue(hasattr(self.user_model, 'updated_at'))
+        self.assertTrue(hasattr(self.user_model, 'email'))
+        self.assertTrue(hasattr(self.user_model, 'password'))
+        self.assertTrue(hasattr(self.user_model, 'first_name'))
+        self.assertTrue(hasattr(self.user_model, 'last_name'))
 
     def test_updated_at_is_public_datetime(self):
-        self.assertEqual(datetime, type(User().updated_at))
+        self.assertEqual(datetime, type(self.user_model.updated_at))
 
     def test_email_is_public_str(self):
-        self.assertEqual(str, type(User.email))
+        self.assertEqual(str, type(self.user_model.email))
 
     def test_password_is_public_str(self):
-        self.assertEqual(str, type(User.password))
+        self.assertEqual(str, type(self.user_model.password))
 
     def test_first_name_is_public_str(self):
-        self.assertEqual(str, type(User.first_name))
+        self.assertEqual(str, type(self.user_model.first_name))
 
     def test_last_name_is_public_str(self):
-        self.assertEqual(str, type(User.last_name))
+        self.assertEqual(str, type(self.user_model.last_name))
 
     def test_two_users_unique_ids(self):
-        us1 = User()
-        us2 = User()
+        us1 = self.user_model
+        us2 = self.user_model
         self.assertNotEqual(us1.id, us2.id)
 
     def test_two_users_different_created_at(self):
-        us1 = User()
+        us1 = self.user_model
         sleep(0.05)
-        us2 = User()
+        us2 = self.user_model
         self.assertLess(us1.created_at, us2.created_at)
 
     def test_two_users_different_updated_at(self):
-        us1 = User()
+        us1 = self.user_model
         sleep(0.05)
-        us2 = User()
+        us2 = self.user_model
         self.assertLess(us1.updated_at, us2.updated_at)
 
     def test_str_representation(self):
         dt = datetime.today()
         dt_repr = repr(dt)
-        us = User()
+        us = self.user_model
         us.id = "123456"
         us.created_at = us.updated_at = dt
         usstr = us.__str__()
@@ -73,10 +75,6 @@ class TestUser_instantiation(unittest.TestCase):
         self.assertIn("'id': '123456'", usstr)
         self.assertIn("'created_at': " + dt_repr, usstr)
         self.assertIn("'updated_at': " + dt_repr, usstr)
-
-    def test_args_unused(self):
-        us = User(None)
-        self.assertNotIn(None, us.__dict__.values())
 
     def test_instantiation_with_kwargs(self):
         dt = datetime.today()
