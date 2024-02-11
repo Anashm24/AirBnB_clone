54415a1bfd36961b7013623c6eda3390e208271d
Anashm24 <anashoumaid19@gmail.com>
2024-02-11 14:11:49 +0100

    new

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,37 +15,43 @@ from models.base_model import BaseModel
 
 class TestBaseModel_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the BaseModel class."""
+    
+    def setUp(self):
+        self.base_model = BaseModel()
+
+    def tearDown(self):
+        del self.base_model
 
     def test_no_args_instantiates(self):
         self.assertEqual(BaseModel, type(BaseModel()))
 
     def test_new_instance_stored_in_objects(self):
-        self.assertIn(BaseModel(), models.storage.all().values())
+        self.assertIn(self.base_model, models.storage.all().values())
 
     def test_id_is_public_str(self):
-        self.assertEqual(str, type(BaseModel().id))
+        self.assertEqual(str, type(self.base_model.id))
 
     def test_created_at_is_public_datetime(self):
-        self.assertEqual(datetime, type(BaseModel().created_at))
+        self.assertEqual(datetime, type(self.base_model.created_at))
 
     def test_updated_at_is_public_datetime(self):
-        self.assertEqual(datetime, type(BaseModel().updated_at))
+        self.assertEqual(datetime, type(self.base_model.updated_at))
 
     def test_two_models_unique_ids(self):
-        bm1 = BaseModel()
-        bm2 = BaseModel()
+        bm1 = self.base_model
+        bm2 = self.base_model
         self.assertNotEqual(bm1.id, bm2.id)
 
     def test_two_models_different_created_at(self):
-        bm1 = BaseModel()
+        bm1 = self.base_model
         sleep(0.05)
-        bm2 = BaseModel()
+        bm2 = self.base_model
         self.assertLess(bm1.created_at, bm2.created_at)
 
     def test_two_models_different_updated_at(self):
-        bm1 = BaseModel()
+        bm1 = self.base_model
         sleep(0.05)
-        bm2 = BaseModel()
+        bm2 = self.base_model
         self.assertLess(bm1.updated_at, bm2.updated_at)
 
     def test_instantiation_with_None_kwargs(self):
@@ -59,6 +65,19 @@ class TestBaseModel_instantiation(unittest.TestCase):
         self.assertEqual(bm.id, "345")
         self.assertEqual(bm.created_at, dt)
         self.assertEqual(bm.updated_at, dt)
+        
+    def test_save_method(self):
+        initial_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        self.assertNotEqual(initial_updated_at, self.base_model.updated_at)
+
+    def test_to_dict_method(self):
+        model_dict = self.base_model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertIn('id', model_dict)
+        self.assertIn('created_at', model_dict)
+        self.assertIn('updated_at', model_dict)
 
 
 if __name__ == "__main__":
